e9d5d87383e5d959a1770e60adb9e3cb4832d33e
gayoung0420 <ciks2508@naver.com>
2021-11-13 19:09:46 +0900

    1113-gayoung

---
+++ 가영/test - 복사본/main.js
@@ -0,0 +1,22 @@
+var http = require("http");
+var express = require("express");
+var fs = require("fs");
+// var mime = require('mime');
+
+var app = express();
+var server = http.createServer(app);
+
+app.use(express.static(__dirname + "/public"));
+console.log(__dirname);
+
+app.get("/", function (req, res) {
+  fs.readFile("./index.html", (err, data) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.send("<img src='/img/img.jpg' />");
+    res.end(data);
+  });
+});
+
+server.listen(80, function () {
+  console.log("sever 작동");
+});
